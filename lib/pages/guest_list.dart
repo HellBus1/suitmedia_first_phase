fe5c6eadda0ce12899e8f25515b46917c4f4b9f6
syubban <syubbanfakhriya@gmail.com>
2021-07-11 17:36:23 +0700

    cached data & pull to request

--- lib/pages/guest_list.dart
+++ lib/pages/guest_list.dart
@@ -1,11 +1,12 @@
 import 'dart:ui';
 
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
-import 'package:suitmedia_first_phase/constant.dart';
 import 'package:suitmedia_first_phase/provider/main_provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class GuestListScreen extends StatefulWidget {
   @override
@@ -15,15 +16,38 @@ class GuestListScreen extends StatefulWidget {
 }
 
 class _StatePage extends State<GuestListScreen> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
   @override
   void initState() {
     super.initState();
     _initialState();
   }
 
+  void _onRefresh() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    Navigator.popAndPushNamed(context, '/guest/list');
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // monitor network fetch
+    await Future.delayed(Duration(milliseconds: 1000));
+    _refreshController.loadComplete();
+  }
+
   _initialState() {
     final provider = Provider.of<MainProvider>(context, listen: false);
-    provider.getDummyGuest().then((value) {});
+    provider.checkConnection().then((value) {
+      if (value) {
+        provider.getDummyGuest().then((value) {});
+      } else {
+        provider.getFileReadAllData().then((value) {});
+      }
+    });
   }
 
   @override
@@ -80,78 +104,106 @@ class _StatePage extends State<GuestListScreen> {
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
                 ),
-                body: Container(
-                    margin: EdgeInsets.only(
-                        top: 20,
-                        bottom: 20,
-                        left: MediaQuery.of(context).size.width / 25,
-                        right: MediaQuery.of(context).size.width / 25),
-                    child: GridView.count(
-                      crossAxisCount: 2,
-                      children:
-                          List.generate(provider.guestList.length, (index) {
-                        final item = provider.guestList[index];
-                        return GestureDetector(
-                          onTap: () {
-                            provider.setGuest(item);
-                            Navigator.pop(context, "success");
-                          },
-                          child: Container(
-                            child: Card(
-                              elevation: 2,
-                              margin: EdgeInsets.only(
-                                  top: 5, bottom: 5, left: 5, right: 5),
-                              child: Container(
-                                child: Stack(
-                                  children: [
-                                    CachedNetworkImage(
-                                        imageUrl:
-                                            'https://qph.fs.quoracdn.net/main-qimg-6291c3a117fc230c82785148baef7eed'),
-                                    ClipRRect(
-                                      // Clip it cleanly.
-                                      child: BackdropFilter(
-                                        filter: ImageFilter.blur(
-                                            sigmaX: 10, sigmaY: 10),
-                                        child: Container(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          alignment: Alignment.center,
-                                          child: Center(
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.center,
-                                              children: [
-                                                Text(
-                                                  item.name,
-                                                  style: TextStyle(
-                                                      color: Colors.white,
-                                                      fontWeight:
-                                                          FontWeight.bold,
-                                                      fontSize: 20),
-                                                ),
-                                                SizedBox(
-                                                  height: 10,
-                                                ),
-                                                Text(item.birthdate,
+                body: SmartRefresher(
+                  enablePullDown: true,
+                  enablePullUp: true,
+                  header: WaterDropHeader(),
+                  footer: CustomFooter(
+                    builder: (BuildContext context, LoadStatus mode) {
+                      Widget body;
+                      if (mode == LoadStatus.idle) {
+                        body = Text("pull up load");
+                      } else if (mode == LoadStatus.loading) {
+                        body = CupertinoActivityIndicator();
+                      } else if (mode == LoadStatus.failed) {
+                        body = Text("Load Failed!Click retry!");
+                      } else if (mode == LoadStatus.canLoading) {
+                        body = Text("release to load more");
+                      } else {
+                        body = Text("No more Data");
+                      }
+                      return Container(
+                        height: 55.0,
+                        child: Center(child: body),
+                      );
+                    },
+                  ),
+                  controller: _refreshController,
+                  onRefresh: _onRefresh,
+                  onLoading: _onLoading,
+                  child: Container(
+                      margin: EdgeInsets.only(
+                          top: 20,
+                          bottom: 20,
+                          left: MediaQuery.of(context).size.width / 25,
+                          right: MediaQuery.of(context).size.width / 25),
+                      child: GridView.count(
+                        crossAxisCount: 2,
+                        children:
+                            List.generate(provider.guestList.length, (index) {
+                          final item = provider.guestList[index];
+                          return GestureDetector(
+                            onTap: () {
+                              provider.setGuest(item);
+                              Navigator.pop(context, "success");
+                            },
+                            child: Container(
+                              child: Card(
+                                elevation: 2,
+                                margin: EdgeInsets.only(
+                                    top: 5, bottom: 5, left: 5, right: 5),
+                                child: Container(
+                                  child: Stack(
+                                    children: [
+                                      CachedNetworkImage(
+                                          imageUrl:
+                                              'https://qph.fs.quoracdn.net/main-qimg-6291c3a117fc230c82785148baef7eed'),
+                                      ClipRRect(
+                                        // Clip it cleanly.
+                                        child: BackdropFilter(
+                                          filter: ImageFilter.blur(
+                                              sigmaX: 10, sigmaY: 10),
+                                          child: Container(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            alignment: Alignment.center,
+                                            child: Center(
+                                              child: Column(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.center,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  Text(
+                                                    item.name,
                                                     style: TextStyle(
                                                         color: Colors.white,
                                                         fontWeight:
-                                                            FontWeight.w400))
-                                              ],
+                                                            FontWeight.bold,
+                                                        fontSize: 20),
+                                                  ),
+                                                  SizedBox(
+                                                    height: 10,
+                                                  ),
+                                                  Text(item.birthdate,
+                                                      style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.w400))
+                                                ],
+                                              ),
                                             ),
                                           ),
                                         ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        );
-                      }),
-                    )),
+                          );
+                        }),
+                      )),
+                ),
               ),
       );
     });
--- lib/provider/main_provider.dart
+++ lib/provider/main_provider.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/widgets.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:suitmedia_first_phase/model/event.dart';
 import 'package:http/http.dart' as http;
 import 'package:suitmedia_first_phase/model/guest.dart';
@@ -10,8 +13,10 @@ class MainProvider extends ChangeNotifier {
   Event event;
   Guest guest;
   bool loadingState = true;
+  bool isConnected;
   List guestList = [];
   String errorMessage = "";
+  final String fileName = "guests.json";
 
   setName(name) {
     this.name = name;
@@ -34,14 +39,12 @@ class MainProvider extends ChangeNotifier {
     // notifyListeners();
 
     try {
+      writeToFile('');
       final response = await http
           .get(Uri.parse('http://www.mocky.io/v2/596dec7f0f000023032b8017'));
       if (response.statusCode == 200) {
-        guestList.clear();
-        jsonDecode(response.body).forEach((data) {
-          guestList.add(Guest.fromJson(data));
-        });
-
+        makeDataList(jsonDecode(response.body));
+        writeToFile(response.body);
         loadingState = false;
         errorMessage = "";
         notifyListeners();
@@ -63,5 +66,74 @@ class MainProvider extends ChangeNotifier {
 
       return false;
     }
+  }
+
+  writeToFile(value) async {
+    String _filePath = await filePath();
+    File userDocumentFile = File(_filePath);
+    userDocumentFile?.writeAsStringSync(value);
+  }
+
+  Future<Directory> documentsPath() async {
+    String tempPath = (await getApplicationDocumentsDirectory())?.path;
+    return Directory("$tempPath").create();
+  }
+
+  Future<String> filePath() async {
+    final path = (await documentsPath()).path;
+    return "$path/$fileName";
+  }
+
+  Future<bool> getFileReadAllData() async {
+    loadingState = true;
+    errorMessage = "";
+
+    try {
+      String _filePath = await filePath();
+      File userDocumentFile = File(_filePath);
+      final data = await userDocumentFile?.readAsString();
+      final jsonData = jsonDecode(data);
+
+      if (jsonData != null) {
+        makeDataList(jsonData);
+
+        loadingState = false;
+        errorMessage = "";
+        notifyListeners();
+        return true;
+      } else {
+        loadingState = false;
+        errorMessage = "Failed to load ";
+        notifyListeners();
+
+        return false;
+      }
+    } catch (e) {
+      print(e);
+      loadingState = false;
+      errorMessage = e.toString();
+      notifyListeners();
+
+      return false;
+    }
+  }
+
+  makeDataList(data) {
+    guestList.clear();
+    data.forEach((data) {
+      guestList.add(Guest.fromJson(data));
+    });
+  }
+
+  Future<bool> checkConnection() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult != ConnectivityResult.none) {
+      this.isConnected = true;
+      notifyListeners();
+      return true;
+    }
+    this.isConnected = false;
+    notifyListeners();
+    return true;
   }
 }
